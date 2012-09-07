b34189fa663360d1e05d31f9fb89ccad1d75d1be
Sascha Watermeyer <saschawatermeyer@gmail.com>
2012-09-07 15:46:45 +0200

    fixed cjax plugins

---
+++ application/response/test.php
@@ -0,0 +1,23 @@
+<?php
+/* Sascha Watermeyer - WTRSAS001
+ * Vulamobi CS Honours project
+ * sascha.watermeyer@gmail.com */
+
+class Test extends CI_Controller 
+{
+    public function __construct() 
+    {
+        parent::__construct();
+    }
+    
+    public function Test() 
+    {
+        //show_404();
+    }
+    
+    public function t()
+    {
+        echo "hello";
+    }
+}
+?>
