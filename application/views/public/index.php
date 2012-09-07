30b5b66d75265eba59a76ebbf3f7cbdfaaf8b2cb
Sascha Watermeyer <saschawatermeyer@gmail.com>
2012-09-07 22:59:12 +0200

    edit demo

--- application/views/public/index.php
+++ application/views/public/index.php
@@ -11,7 +11,7 @@
     <input type="password" id="password" name="password"></input>
 </p>
 <p>
-    <input type="submit" name="submit" id="login" value="login" onclick="login()" >
+    <input type="submit" name="submit" id="login" value="login" onclick="login()" ><pre><a href="ajax.php?auth/login">link</a></pre>
 </p>
 </br>
 <h2>AJAX Functions</h2>
@@ -19,31 +19,31 @@
     Test out the jQuery AJAX calls to the VulaMobi Backend hosted on nightmare
 </p>
 <p>
-    <input type="submit" name="submit" id="logout" value="logout" onclick="logout()">
+    <input type="submit" name="submit" id="logout" value="logout" onclick="logout()"><pre><a href="ajax.php?auth/logout">link</a></pre>
 </p>
 <p>
-    <input type="submit" name="submit" id="name" value="name" onclick="username();">
-</p>
-<p>
-    <!-- site_id for CS Honours 2012 -->
-    <input type="submit" name="submit" id="sites" value="sites" onclick="sites();">
-</p>
-<p>
-    <input type="submit" name="submit" id="id" value="id" onclick="user_id();">
+    <input type="submit" name="submit" id="name" value="name" onclick="username();"><pre><a href="ajax.php?student/name">link</a></pre>
 </p>
 <p>
     <!-- site_id for CS Honours 2012 -->
-    <input type="submit" name="submit" id="grade" value="grade" onclick="grade('fa532f3e-a2e1-48ec-9d78-3d5722e8b60d');"> <b>CS Honours 2012</b>
+    <input type="submit" name="submit" id="sites" value="sites" onclick="sites();"><pre><a href="ajax.php?student/sites">link</a></pre>
 </p>
 <p>
-    <input type="submit" name="submit" id="gallery" value="gallery" onclick="gallery();">
+    <input type="submit" name="submit" id="id" value="id" onclick="user_id();"><pre><a href="ajax.php?student/id">link</a></pre>
 </p>
 <p>
     <!-- site_id for CS Honours 2012 -->
-    <input type="submit" name="submit" id="role" value="role" onclick="role('fa532f3e-a2e1-48ec-9d78-3d5722e8b60d');"><b>CS Honours 2012</b>
+    <input type="submit" name="submit" id="grade" value="grade" onclick="grade('fa532f3e-a2e1-48ec-9d78-3d5722e8b60d');"> <b>CS Honours 2012</b><pre><a href="ajax.php?grade/site/fa532f3e-a2e1-48ec-9d78-3d5722e8b60d">link</a></pre>
 </p>
 <p>
-    <input type="submit" name="submit" id="roster" value="roster" onclick="roster('fa532f3e-a2e1-48ec-9d78-3d5722e8b60d');"><b>CS Honours 2012</b>
+    <input type="submit" name="submit" id="gallery" value="gallery" onclick="gallery();"><pre><a href="ajax.php?gallery/dir">link</a></pre>
+</p>
+<p>
+    <!-- site_id for CS Honours 2012 -->
+    <input type="submit" name="submit" id="role" value="role" onclick="role('fa532f3e-a2e1-48ec-9d78-3d5722e8b60d');"><b>CS Honours 2012</b><pre><a href="ajax.php?role/site/fa532f3e-a2e1-48ec-9d78-3d5722e8b60d">link</a></pre>
+</p>
+<p>
+    <input type="submit" name="submit" id="roster" value="roster" onclick="roster('fa532f3e-a2e1-48ec-9d78-3d5722e8b60d');"><b>CS Honours 2012</b><pre><a href="ajax.php?role/roster/fa532f3e-a2e1-48ec-9d78-3d5722e8b60d">link</a></pre>
 </p>
 <h4><a href="https://github.com/zuch/VulaMobi/wiki">documentation</a></h4>
 <h2>Source Code</h2>
